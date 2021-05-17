52ccd1ac8bf97b4ce854277757037db03abb6023
jngsjn <firstarting@gmail.com>
2021-05-17 09:20:26 +0900

    edit views.py

--- django_crud/django_crud/blog/views.py
+++ django_crud/django_crud/blog/views.py
@@ -19,8 +19,11 @@ def create(request):
   new_blog.title = request.POST['title']
   new_blog.writer = request.POST['writer']
   new_blog.body = request.POST['body']
-  new_blog.image = request.FILES['image']
   new_blog.pub_date = timezone.now()
+  try:
+    new_blog.image = request.FILES['image']
+  except:
+    pass
   new_blog.save()
 
   return redirect('detail',new_blog.id)
